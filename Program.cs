c71a4d060b0a9e007265b551c7e9ea5d2125eb01
Grzegorz <grzegorzokon@gmail.com>
2021-10-17 10:20:03 +0200

    Inicjalizacja projektu

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace session_unlocker
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            if (args.Length > 0 && args.Contains("-V")) {
+                Console.WriteLine(Version.getVersionInfo());
+            } else {
+                Console.WriteLine("Startujemy...");
+            }
+        }
+    }
+}
---
+++ Version.cs
@@ -0,0 +1,14 @@
+﻿namespace session_unlocker
+{
+    class Version {
+        private static string name = "Session Unlocker";
+        private static int major = 1;
+        private static int minor = 0;
+        private static int release = 0;
+        private static string revision = "17 October 2021";
+
+        public static string getVersionInfo() {
+            return name + " " + major + "." + minor + "." + release + " (revision " + revision + ")";
+        }
+    }
+}
